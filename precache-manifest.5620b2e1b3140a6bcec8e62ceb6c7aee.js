c0437a001136410f87f2d1e2baa7a1146eccc869
Grace Nduta <45939062+ndutagrace25@users.noreply.github.com>
2019-07-08 10:51:59 +0300

    Updates

---
+++ precache-manifest.5620b2e1b3140a6bcec8e62ceb6c7aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10404bd62387692b53ba9c7f150c6de9",
+    "url": "/contactmanager_redux/index.html"
+  },
+  {
+    "revision": "1d6e848101fdd9ca6d20",
+    "url": "/contactmanager_redux/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9d755793b569be3c2769",
+    "url": "/contactmanager_redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1d6e848101fdd9ca6d20",
+    "url": "/contactmanager_redux/static/js/2.2534555f.chunk.js"
+  },
+  {
+    "revision": "9d755793b569be3c2769",
+    "url": "/contactmanager_redux/static/js/main.6083fe3c.chunk.js"
+  },
+  {
+    "revision": "0b64d2f3d80b19226e3f",
+    "url": "/contactmanager_redux/static/js/runtime~main.1b5be53f.js"
+  }
+]);
